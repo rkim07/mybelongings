36c3ab1c764a3673c39fafc99051ae38e6c52d69
kimr234z <ryan.kim2@panasonic.aero>
2021-01-21 01:38:10 -0800

    remove gitignore files

--- build/app/config/NotificationManager.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NotificationManager = void 0;
-class NotificationManager {
-    constructor() {
-    }
-}
-exports.NotificationManager = NotificationManager;
-//# sourceMappingURL=NotificationManager.js.map
